6c7ab54e50350941bd321d5039f078574375cf2c
Jacob See <jacob.d.see@gmail.com>
2020-03-12 16:11:41 -0700

    Add registry parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-echo "$5" | docker login --username "$4" --password-stdin
+echo "$5" | docker login "$6" --username "$4" --password-stdin
 docker pull "$2"
 s2i build "$1" "$2" "$3"
