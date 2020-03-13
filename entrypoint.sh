4f6e27c29e73a42a5b1323184a6f3f8ff8da4bbe
Jacob See <jacob.d.see@gmail.com>
2020-03-12 17:59:59 -0700

    Think this through a little more...

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,4 @@ echo "$6" | docker login "$4" --username "$5" --password-stdin
 echo "$9" | docker login "$7" --username "$8" --password-stdin
 docker pull "$2"
 s2i build "$1" "$2" "$3"
-docker tag "$3" "$7/$3"
-docker push "$7/$3"
+docker push "$3"
