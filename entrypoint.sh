cf373e7960a06e05ea91f4ed1b121d05927e2929
Jacob See <jacob.d.see@gmail.com>
2020-03-12 17:32:49 -0700

    Add tag & push action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 set -e
-echo "$5" | docker login "$6" --username "$4" --password-stdin
+echo "$6" | docker login "$4" --username "$5" --password-stdin
+echo "$9" | docker login "$7" --username "$8" --password-stdin
 docker pull "$2"
 s2i build "$1" "$2" "$3"
+docker tag "$3" "$7/$3"
+docker push "$7/$3"
